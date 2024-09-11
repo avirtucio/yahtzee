ab044141d647187377db39c7cb4b6679a47d4c33
avirtucio <160163737+avirtucio@users.noreply.github.com>
2024-09-11 12:57:38 -0400

    Add files via upload

---
+++ Yahtzee/server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import json
+import math
+import os
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def login():
+
+    return render_template('login.html')
+
+@app.route('/game')
+def game():
+    username = request.args.get('username')
+
+    return render_template('game.html', username=username)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("GET", 8080))
+    app.run(debug=True, port=port)
