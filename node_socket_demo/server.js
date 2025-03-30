32a14e9c9d6581f79667f87c19813517a1ab92b4
avirtucio <arlo.virtucio25@trinityschoolnyc.org>
2025-03-30 18:25:22 -0400

    valid_score_entry FULLY works

--- node_socket_demo/server.js
+++ node_socket_demo/server.js
@@ -36,9 +36,9 @@ io.on('connection', function(socket){
   });
 
   socket.on('valid_score_entry', async function(data){
-    console.log("valid score entry for "+ data.username)
-    console.log(data.username+"'s categories", data.user_categories)
-    console.log(data.game_name)
+    // console.log("valid score entry for "+ data.username)
+    // console.log(data.username+"'s categories", data.user_categories)
+    // console.log(data.game_name)
 
     scorecard_name = data.game_name+"|"+data.username
 
@@ -60,6 +60,37 @@ io.on('connection', function(socket){
     } catch (error) {
         console.error(error.message);
     }
+    
+    console.log("server.js, valid_score_entry, after scorecard update")
+    let all_game_scorecards;
+    
+    try {
+      const response = await fetch(`http://127.0.0.1:8080/gameDataToNodeServer/${data.game_name}`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              game_name: data.game_name
+          })
+      });
+
+      if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log("server.js, valid_score_entry, all_games_scorecards", json)
+      all_game_scorecards = json;
+      
+    } catch (error) {
+        console.error(error.message);
+    }
+
+    io.to(data.game_name).emit('valid_score_entry', {
+      message: data.username+" has submitted a new score",
+      all_game_scorecards: all_game_scorecards
+    });
 
   });
 
@@ -88,7 +119,7 @@ app.get('/games/:game_name/:username', async function(request, response) {
       }
 
       const json = await response.json();
-      // console.log(json)
+      // console.log("server.js, all_games_scorecards", json)
       all_game_scorecards = json;
       
     } catch (error) {
